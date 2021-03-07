5b05835a700da66ff33cfd79c36b1dac56f4cd0c
Eyal Dar <eyaldar2014@gmail.com>
2021-03-07 15:34:19 +0200

    restart left td

--- js/index.js
+++ js/index.js
@@ -4,22 +4,34 @@ const pictureApi = 'https://dog.ceo/api/breeds/image/random'
 const board = document.querySelector('#board')
 const body = document.querySelector('body')
 const count = document.querySelector('#count')
+const landing = document.querySelector('#landing')
+const minute = document.querySelector("#minutes")
+const second = document.querySelector("#seconds")
+const champion = document.querySelector("#champion")
+const hiddenTextBox = document.querySelector("#hiddenTextBox")
+const championName = document.querySelector("#championName")
 
 
 const randomArray = []
 const randomCardsArray = []
 let numOfOpenCards = 0
 let wrongCounter = 0
+let rightCounter = 0
+let record = 0
+let number
 let temp
 let current
+let gameOn = false
+let stopWach = false
 // console.log(board)
 
 
 
-
-
-
+/* functions ------------------------------------------------------------------------*/
 function createCardCouple(n) {
+
+  landing.style.zIndex = '-1'
+  number = n
 
   for (let i = 0; n > i; i++) {
 
@@ -83,6 +95,11 @@ function randomize() {
 
 function revealCard(e) {
 
+  if (gameOn === false) {
+    clockWise()
+    gameOn = true
+  }
+
   if (numOfOpenCards === 0) {
     current = e.target
     temp = current
@@ -99,7 +116,17 @@ function revealCard(e) {
     if (temp.style.backgroundImage === current.style.backgroundImage) {
       current.removeEventListener('click', revealCard)
       temp.removeEventListener('click', revealCard)
+      rightCounter += 1
       numOfOpenCards = 0
+      if(rightCounter === number){
+        stopWach = true
+        const win = document.createElement('div')
+        win.innerHTML = ' YOU WON !!!'
+        win.classList.add('youWon')
+        board.appendChild(win)
+        checkRecord()
+
+      }
     }
     else {
       numOfOpenCards = 2
@@ -116,17 +143,62 @@ function flipBack() {
   numOfOpenCards = 0
 }
 
+function clockWise() {
+
+  if (stopWach === true){
+    return
+  }
+
+  let newValue = parseInt(second.innerHTML)
+  let minuteValue = parseInt(minute.innerHTML)
+
+  newValue += 1
+  if (newValue < 10) {
+    newValue = '0' + newValue.toString()
+  }
+
+  else if (newValue === 60) {
+    newValue = '00'
+
+    minuteValue += 1
+  }
+  if (minuteValue < 10) {
+    minuteValue = '0' + minuteValue.toString()
+  }
+
+  else if (minuteValue === 60) {
+    minuteValue = '00'
+    return
+  }
+
+  second.innerHTML = newValue
+  minute.innerHTML = minuteValue
+
+  setTimeout(clockWise, 1000)
+}
 
 
 
+// const allDivs = document.querySelectorAll('div')
+// allDivs.forEach(div => {
+//   div.style.border = "1px solid red"
 
+// })
 
+// createCardCouple(1)
+function checkRecord(){
+  let score = minutes.innerHTML + second.innerHTML
+  if (parseInt(score) > record){
+    record = parseInt(score)
+    // champion.innerHTML = score
+    hiddenTextBox.style.zIndex ='100'
 
-const allDivs = document.querySelectorAll('div')
-allDivs.forEach(div => {
-  div.style.border = "1px solid red"
+  }
 
-})
+}
 
-createCardCouple(2)
-
+function applyRecord(){
+  console.log(championName.value)
+  champion.innerHTML = championName.value
+  hiddenTextBox.style.zIndex ='-100'
+}
